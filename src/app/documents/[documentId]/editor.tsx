dae13762e5c62aff1c1c6c403ccaacba2ac1b4be
Mathi Ramilo <mathiramilo2290@gmail.com>
2025-08-23 18:07:36 -0300

    add extensions, ruler and navbar

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,25 +1,33 @@
-'use client';
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import Image from '@tiptap/extension-image';
-import ImageResize from 'tiptap-extension-resize-image';
-import FontFamily from '@tiptap/extension-font-family';
-import TextStyle from '@tiptap/extension-text-style';
-import Underline from '@tiptap/extension-underline';
-import TaskItem from '@tiptap/extension-task-item';
-import TaskList from '@tiptap/extension-task-list';
-import Table from '@tiptap/extension-table';
-import TableCell from '@tiptap/extension-table-cell';
-import TableHeader from '@tiptap/extension-table-header';
-import TableRow from '@tiptap/extension-table-row';
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Color from "@tiptap/extension-color";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import TextAlign from "@tiptap/extension-text-align";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import FontFamily from "@tiptap/extension-font-family";
+import TextStyle from "@tiptap/extension-text-style";
+import Underline from "@tiptap/extension-underline";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
 
-import { useEditorStore } from '@/store/use-editor-store';
+import { useEditorStore } from "@/store/use-editor-store";
+import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -46,18 +54,33 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: 'padding-left: 56px; padding-right: 56px;',
+        style: "padding-left: 56px; padding-right: 56px;",
         class:
-          'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
     extensions: [
       StarterKit,
+      FontSizeExtension,
+      LineHeightExtension.configure({
+        types: ["paragraph", "heading"],
+        defaultLineHeight: "normal",
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
+      TextStyle,
+      FontFamily,
+      Underline,
+      Color,
+      Highlight.configure({ multicolor: true }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
       Image,
       ImageResize,
-      FontFamily,
-      TextStyle,
-      Underline,
       TaskItem.configure({
         nested: true,
       }),
@@ -75,7 +98,7 @@ export const Editor = () => {
               <th colspan="3">Description</th>
             </tr>
             <tr>
-              <td>Cyndi Lauper</td>
+              <td>John Stones</td>
               <td>Singer</td>
               <td>Songwriter</td>
               <td>Actress</td>
@@ -87,6 +110,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Editor } from './editor';
-import { Toolbar } from './toolbar';
+import React from "react";
+import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
+import { Navbar } from "./navbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
@@ -11,8 +12,13 @@ export default async function DocumentPage({ params }: DocumentIdPageProps) {
 
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 }
