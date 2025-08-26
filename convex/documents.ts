0ac6ab8a45ebf2427c60f9d09d877599c04ba88d
Mathi Ramilo <mathiramilo2290@gmail.com>
2025-08-25 22:57:43 -0300

    add remove and rename document

--- convex/documents.ts
+++ convex/documents.ts
@@ -29,3 +29,49 @@ export const get = query({
     return await ctx.db.query("documents").paginate(args.paginationOpts);
   },
 });
+
+export const removeById = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const document = await ctx.db.get(args.id);
+
+    if (!document) {
+      throw new ConvexError("Document not found");
+    }
+
+    if (document.ownerId !== user.subject) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const updateById = mutation({
+  args: { id: v.id("documents"), title: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const document = await ctx.db.get(args.id);
+
+    if (!document) {
+      throw new ConvexError("Document not found");
+    }
+
+    if (document.ownerId !== user.subject) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    await ctx.db.patch(args.id, { title: args.title });
+  },
+});
